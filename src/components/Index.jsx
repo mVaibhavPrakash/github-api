14c8cff28c60ad0b91157a8d653b9d4aadfb1531
Vaibhav prakash <vp1997dav@gmail.com>
2022-06-16 21:44:44 +0530

    Added gh-pages

--- src/components/Index.jsx
+++ src/components/Index.jsx
@@ -1,7 +1,7 @@
 import ReactDOM from 'react-dom/client';
 import {BrowserRouter} from 'react-router-dom'
-import App from './components/App';
-import './css/index.css'
+import App from './App';
+import '../css/index.css'
 
 const root = document.getElementById('root')
 ReactDOM.createRoot(root).render(
