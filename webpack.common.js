164aaa7455b147bdbd3c598a0edd8c28858bab4b
Vaibhav prakash <vp1997dav@gmail.com>
2022-06-16 22:46:13 +0530

    deployed to netlify

--- webpack.common.js
+++ webpack.common.js
@@ -5,7 +5,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 module.exports = {
   entry: './src/components/Index.jsx',
   output: {
-    path: path.resolve(__dirname, 'build'),
+    path: path.resolve(__dirname, 'build/'),
     filename: 'clientbundle.js',
     assetModuleFilename: 'img/[hash]][ext][query]',
   },
